319a0af68ac68e76b0d532dc894bc386a5ec8323
jerry23 <baojiedashuaibi@gmail.com>
2020-07-05 23:26:03 +0800

    feat: new

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: '前端成长之路',
   description: '构建知识体系，并分类整理对应的技术实现。',
   themeConfig: {
-    sidebar: ['/summary/', '/network/'],
+    sidebar: ['/summary/', '/network/', '/algo/'],
   },
   dest: 'public'
 };
